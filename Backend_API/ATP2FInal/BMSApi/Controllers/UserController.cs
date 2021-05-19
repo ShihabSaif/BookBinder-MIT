632d8e471cd254c49840905d88ab7f04f558f204
Meghla <meghla515@gmail.com>
2021-05-19 19:13:39 +0600

    file upload

--- Backend_API/ATP2FInal/BMSApi/Controllers/UserController.cs
+++ Backend_API/ATP2FInal/BMSApi/Controllers/UserController.cs
@@ -11,7 +11,9 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Hosting;
 using System.Web.Http;
@@ -77,6 +79,48 @@ namespace BMSApi.Controllers
         {
             repo.Insert(bk);
             return Created("", bk);
+        }
+
+        [System.Web.Http.Route("upload")]
+        [System.Web.Http.HttpPost]
+        public Task<HttpResponseMessage> Upload()
+        {
+            List<string> savedFilePath = new List<string>();
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+            string rootPath = HttpContext.Current.Server.MapPath("~/uploadFiles");
+            var provider = new MultipartFileStreamProvider(rootPath);
+            var task = Request.Content.ReadAsMultipartAsync(provider).
+                ContinueWith<HttpResponseMessage>(t =>
+                {
+                    if (t.IsCanceled || t.IsFaulted)
+                    {
+                        Request.CreateErrorResponse(HttpStatusCode.InternalServerError, t.Exception);
+                    }
+                    foreach (MultipartFileData item in provider.FileData)
+                    {
+                        try
+                        {
+                            string name = item.Headers.ContentDisposition.FileName.Replace("\"", "");
+                            string newFileName = Guid.NewGuid() + Path.GetExtension(name);
+                            File.Move(item.LocalFileName, Path.Combine(rootPath, newFileName));
+
+                            Uri baseuri = new Uri(Request.RequestUri.AbsoluteUri.Replace(Request.RequestUri.PathAndQuery, string.Empty));
+                            string fileRelativePath = "~/uploadFiles/" + newFileName;
+                            Uri fileFullPath = new Uri(baseuri, VirtualPathUtility.ToAbsolute(fileRelativePath));
+                            savedFilePath.Add(fileFullPath.ToString());
+                        }
+                        catch (Exception ex)
+                        {
+                            string message = ex.Message;
+                        }
+                    }
+
+                    return Request.CreateResponse(HttpStatusCode.Created, savedFilePath);
+                });
+            return task;
         }
 
         //[System.Web.Http.HttpPost]
